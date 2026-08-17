640b4a8805f8ba8e666e8294d98c20e7b5c919de
Meerab <meerabc955@gmail.com>
2026-08-17 11:54:07 +0500

    Initial commit: blank portfolio page

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '20vh' }}>
+      <h1>Meerab Chaudhary</h1>
+      <p>Portfolio coming soon.</p>
+    </div>
+  )
+}
+
+export default App
