1ac205cc668c5867ed9fa0cef46cdb8f23ba1f2e
Meerab <meerabc955@gmail.com>
2026-08-20 19:17:30 +0500

    add vercel analutics feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Layout from './components/Layout.jsx'
 import Home from './pages/Home.jsx'
 import Work from './pages/Work.jsx'
@@ -7,14 +8,17 @@ import Contact from './pages/Contact.jsx'
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+      <Analytics />
+    </>
   )
 }
 
